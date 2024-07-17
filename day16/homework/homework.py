6e05bfa318bb8d30d52485b6382c5e11178fc993
sheshaberidze24 <sheshaberidzenika4@gmail.com>
2024-07-17 09:15:37 +0400

    first commit

--- day16/homework/homework.py
+++ day16/homework/homework.py
@@ -1,6 +1,16 @@
+# 1
 num1 = [5, 6, 7, 3, 1, 9, 2]
 min = num1[0]
 for i in num1:
     if min > i:
         min = i
 print(min)
+
+# 2
+num2 = [5, 6, 7, 3, 1, 9, 2]
+max = num2[0]
+for i in num2:
+    if max < i:
+        max = i
+print(max)
+
