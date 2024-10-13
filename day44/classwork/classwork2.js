f1971789ac61722da23cecc397ff77d8277c0932
sheshaberidze24 <sheshaberidzenika4@gmail.com>
2024-10-13 20:15:00 +0400

    sixth comit

---
+++ day44/classwork/classwork2.js
@@ -0,0 +1,25 @@
+// 2
+function multiplay(a, b, c) {
+    return a * b * c;
+}
+
+let multyplication = multiplay(12, 69, 11);
+document.getElementById("p1").textContent - "the result is" + multyplication
+
+
+// 2
+function sandwitch(ing, ing2, ing3){
+    console.log("me gavakete sandwich am ingredientebit" + ing + " " + ing2 + " " + ing3
+
+    )
+}
+sandwitch("yveli", "puri", "kalbasi")
+
+// 3
+
+
+// function check(num){
+//     if (num % 2 == 0){
+//         return true
+//     }
+// }
